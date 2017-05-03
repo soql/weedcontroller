1a479878a8263328590ca059b676b7655a12d009
Piotr Sokolowski <Piotr.Sokolowski@orange.com>
2017-05-03 09:44:49 +0200

    Poprawiony log4j

--- yo/src/components/Main.js
+++ yo/src/components/Main.js
@@ -125,7 +125,7 @@ class Main extends React.Component {
         {this.renderSwitches()}
       </BlockUi>     
       </div>
-      <a href="<c:url value="/logout" />">Logout</a>
+      <a href="logout">Logout</a>
     </div>     
     
     )
