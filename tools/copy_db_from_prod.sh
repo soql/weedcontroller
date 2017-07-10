7459a78e726590e7d7b59337fe967df4dcbfae52
Piotr Sokolowski <Piotr.Sokolowski@orange.com>
2017-07-10 07:21:13 +0200

    Wstępne development tabelki z logami audytowymi.

---
+++ tools/copy_db_from_prod.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -u weedcontroller -p weedcontroller < baza
---
+++ yo/src/components/AuditTable.js
@@ -0,0 +1,57 @@
+require('bootstrap/dist/css/bootstrap.css');
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+
+
+import React from 'react';
+import axios from 'axios';
+import {Button} from 'reactstrap';
+import AppActions from '../actions/AppActions';
+import LogStore from '../stores/LogStore';
+
+class AuditTable extends React.Component {
+  constructor (props) {
+    super(props) ;
+    this.state = {logs: []};
+  }
+     
+  componentWillMount(){
+	  AuditStore.addChangeListener('STORE_LOG_CHANGED', this.logChanged.bind(this));
+  }
+  
+  logChanged(){		  
+	  this.setState({logs: LogStore.getLogs()});	 
+  } 
+  renderLog(element){
+  	return (
+  			<tr className="AuditTableTr">  			
+  			<td className="AuditTableTd">{element.name}</td>
+  			<td className="AuditTableTd">{element.time}</td>
+  			</tr>);
+  }
+  
+  renderAudit(){
+	  let rows=[];	  	  
+	  this.state.logs.forEach((element) => {
+		  rows.push(this.renderLog(element));				  
+	  });
+	  return (
+			  <table className="AuditTable">
+			    {rows}
+			  	<tr><td colSpan="4"><Button color="primary" size="1g" block onClick={this.showMoreLogs.bind(this)}>...</Button></td></tr>
+			  </table>
+			 );   	    	  
+  }
+  showMoreLogs(){
+	  AppActions.showMoreLogs();
+  }
+  
+  render(){
+	  return this.renderLogs()      	
+  }
+  
+ 
+}
+
+export default AuditTable;
