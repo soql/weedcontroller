2015ed2713d09cfe9d7b32b684c99aa28164e33c
psokolowski <piotr.sokolowski@gmail.com>
2018-02-07 23:09:31 +0100

    Test

--- src/test/java/pl/net/oth/weedcontroller/StartDateTest.java
+++ src/test/java/pl/net/oth/weedcontroller/StartDateTest.java
@@ -1,6 +1,6 @@
 package pl.net.oth.weedcontroller;
 
-
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -40,9 +40,14 @@ import pl.net.oth.weedcontroller.task.ruletask.Command;
 import pl.net.oth.weedcontroller.task.ruletask.RulesTask;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {UserService.class, PhaseDAO.class, PhaseService.class, SensorDAO.class, SensorService.class, RulesTask.class, TestConfig.class, RulesTask.class, Command.class, GpioMockExternalController.class, ConfigurationService.class, ConfigurationDAO.class, SMSController.class, RulesTask.class, SwitchService.class, SwitchDAO.class, SwitchLogDAO.class, UserDAO.class, GpioMockExternalController.class, SensorTask.class, SensorExternalController.class, RuleService.class, RuleDAO.class, SensorResultService.class, SensorResultDAO.class })
+@ContextConfiguration(classes = { UserService.class, PhaseDAO.class, PhaseService.class, SensorDAO.class,
+		SensorService.class, RulesTask.class, TestConfig.class, RulesTask.class, Command.class,
+		GpioMockExternalController.class, ConfigurationService.class, ConfigurationDAO.class, SMSController.class,
+		RulesTask.class, SwitchService.class, SwitchDAO.class, SwitchLogDAO.class, UserDAO.class,
+		GpioMockExternalController.class, SensorTask.class, SensorExternalController.class, RuleService.class,
+		RuleDAO.class, SensorResultService.class, SensorResultDAO.class })
 @ActiveProfiles(profiles = "DEV")
-@DirtiesContext(classMode=ClassMode.AFTER_CLASS)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class StartDateTest {
 	@Autowired
 	private SensorService sensorService;
@@ -50,33 +55,51 @@ public class StartDateTest {
 	private SensorResultService sensorResultService;
 	@Autowired
 	private Command command;
-	
-	@Test		
+
+	@Test
 	public void test() {
-		 Sensor s=sensorService.getSensorByNumber(3);
-		 System.out.println(s.getName());
-		List<SensorResultLog> ser=sensorResultService.getResultsForDate(new Date(new Date().getTime()-1000*60*60*24*15), new Date(), s);
+		Sensor s = sensorService.getSensorByNumber(3);
+		System.out.println(s.getName());
+		List<SensorResultLog> ser = sensorResultService
+				.getResultsForDate(new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 15), new Date(), s);
 		System.out.println(ser.size());
-		SensorResultLog[] list=new SensorResultLog[ser.size()];
+		SensorResultLog[] list = new SensorResultLog[ser.size()];
 		ser.toArray(list);
 		System.out.println(list.length);
-		int SEC=1000;
-		for(int i=0; i<list.length;) {
-			for(int j=0; j<list.length;) {					
-				if(list[j].getDate().getTime()-list[i].getDate().getTime()<600*SEC) {
+		int SEC = 1000;
+		boolean active=false;
+		for (int i = 0; i < list.length;) {
+			for (int j = 0; j < list.length;) {
+				if (list[j].getDate().getTime() - list[i].getDate().getTime() < 900 * SEC) {
 					j++;
 					continue;
 				}
-			/*	System.out.println("JEST "+j+" do "+i+" = "+(list[j].getDate().getTime()-list[i].getDate().getTime())/SEC);*/
-			/*	System.out.println("HuMI"+list[j].getHumidity()+" -> "+list[i].getHumidity()+" ("+list[j].getHumidity()-list[i].getHumidity()+")");*/
-				if(list[j].getHumidity()-list[i].getHumidity()>300) {
-					System.out.println("JEST "+j+" do "+i+" = "+(list[j].getDate().getTime()-list[i].getDate().getTime())/SEC);
-					System.out.println("HuMI"+list[j].getHumidity()+" -> "+list[i].getHumidity());
-					System.out.println("Wykryto podlanie: "+list[j].getHumidity()+" -> "+list[i].getHumidity());
+				/*
+				 * System.out.println("JEST "+j+" do "+i+" = "+(list[j].getDate().getTime()-list
+				 * [i].getDate().getTime())/SEC);
+				 */
+				/*
+				 * System.out.println("HuMI"+list[j].getHumidity()+" -> "+list[i].getHumidity()
+				 * +" ("+list[j].getHumidity()-list[i].getHumidity()+")");
+				 */
+				if (list[i].getHumidity() - list[j].getHumidity() > 300) {
+					if(!active) {
+						System.out.println("JEST " + formatDate(list[i].getDate()) + " do " + formatDate(list[j].getDate())
+								+ " = " + (list[j].getDate().getTime() - list[i].getDate().getTime()) / SEC);
+						System.out.println("Wykryto podlanie: " + list[j].getHumidity() + " -> " + list[i].getHumidity()
+								+ "(" + (list[i].getHumidity() - list[j].getHumidity()) + ")");					
+						active=true;
+					}
+				}else {
+					active=false;
 				}
 				i++;
 			}
 		}
 		System.exit(0);
+	}
+
+	public String formatDate(Date date) {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date);
 	}
 }
