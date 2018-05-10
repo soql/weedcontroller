0b804b6cf57a1b45fcd3234304faa804b96c9e20
psokolowski <piotr.sokolowski@gmail.com>
2018-05-10 08:32:52 +0200

    Poprawka skryptu do czyszczenia bazy danych.

--- tools/clear_database.sql
+++ tools/clear_database.sql
@@ -1,3 +1,5 @@
 truncate table weedcontroller.AuditLog;
 truncate table weedcontroller.SensorResultLog;
 truncate table weedcontroller.SwitchLog;
+truncate table weedcontroller.SwitchGpioLog;
+truncate table weedcontroller.ChangeDetection
