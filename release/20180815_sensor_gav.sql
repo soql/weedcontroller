2ad345d567643a5df37e2e50749dde3a370183d0
psokolowski <piotr.sokolowski@cgi.com>
2018-08-16 10:10:08 +0200

    poprawka sql

--- release/20180815_sensor_gav.sql
+++ release/20180815_sensor_gav.sql
@@ -1,3 +1,8 @@
 update weedcontroller.Rule set expression_=replace(expression_, "r.gavAS(", "r.gavAS('Wewnątrz',") where expression_ like '%r.gavAS(%';
 update weedcontroller.Rule set expression_=replace(expression_, "r.gav(", "r.gav('Wewnątrz',") where expression_ like '%r.gav(%';
+
+update weedcontroller.Rule set expression_=replace(expression_, '"HUMI"', '"HUMIDITY"') where expression_ like '%"HUMI"%';
+update weedcontroller.Rule set expression_=replace(expression_, '"TEMP"', '"TEMPERATURE"') where expression_ like '%"TEMP"%';
+
 update weedcontroller.Rule set condition_=replace(condition_, "r.gav(", "r.gav('Wewnątrz',") where condition_ like '%r.gav%';
+update weedcontroller.Rule set condition_=replace(expression_, '"HUMI"', '"HUMIDITY"') where condition_ like '%"HUMI"%';
